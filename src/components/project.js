e9c90fad9a3ef0e5f028556f4f533061ab7dfedf
Priyanshu <priyanshusingh.0601@gmai.com>
2024-09-23 12:02:26 +0530

    Add full stack project

--- src/components/project.js
+++ src/components/project.js
@@ -1,9 +1,9 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./project.css";
-import holiImg from './images/holi.jpeg';
-import tourImg from './images/tour.jpeg';
-import deliciasoImg from './images/deliciaso.jpeg';
-
+import dailyblog from "./images/Dailyblog (2).png";
+import tourImg from "./images/tour.jpeg";
+import deliciasoImg from "./images/deliciaso.jpeg";
 
 export default function Navbar() {
   return (
@@ -12,19 +12,60 @@ export default function Navbar() {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       />
-
       <div className="project" id="project">
         <div className="container">
           <div className="heading">
             <p>Projects</p>
             <h3>Each project is a unique piece of development 🧩</h3>
           </div>
-
           <div className="block">
-            <div className="siteImage">
+            <div className="aboutProject">
+              <h4>
+                <b>Dail Blog</b>
+              </h4>
+              <p>
+                A full-stack web application that allows users to create
+                accounts, post blogs with images, and engage with content by
+                commenting on blogs and also replying to the comments.
+              </p>
+              <div className="techName">
+                <p>React</p>
+                <p>Nodejs</p>
+              </div>
+              <div class="links">
+                <a
+                  href="https://github.com/Priyanshu-su30/blog-frontend?tab=readme-ov-file"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  CODE <i className="fa fa-fw fa-github"></i>
+                </a>
+                <a
+                  target="_blank"
+                  href="https://priyanshu-dailyblog.netlify.app/"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                  <i class="fa-solid fa-arrow-up-right-from-square link-icon"></i>
+                </a>
+              </div>
+            </div>
+            <div className="siteImage" id="project1">
+              <a
+                target="_blank"
+                href="https://priyanshu-dailyblog.netlify.app/"
+                rel="noreferrer"
+              >
+                <img src={dailyblog} />
+              </a>
+            </div>
+          </div>
+          <div className="block">
+            <div className="siteImage" id="project2">
               <a
                 target="_blank"
                 href="https://priyanshu-su30.github.io/TOUR/index.html"
+                rel="noreferrer"
               >
                 <img src={tourImg} />
               </a>
@@ -47,6 +88,7 @@ export default function Navbar() {
                 <a
                   href="https://github.com/Priyanshu-su30/TOUR"
                   target="_blank"
+                  rel="noreferrer"
                 >
                   CODE <i className="fa fa-fw fa-github"></i>
                 </a>
@@ -82,6 +124,7 @@ export default function Navbar() {
                 <a
                   href="https://github.com/Priyanshu-su30/DELICIASO"
                   target="_blank"
+                  rel="noreferrer"
                 >
                   CODE <i className="fa fa-fw fa-github"></i>
                 </a>
@@ -95,54 +138,14 @@ export default function Navbar() {
                 </a>
               </div>
             </div>
-            <div className="siteImage">
+            <div className="siteImage" id="project3">
               <a
                 target="_blank"
                 href="https://priyanshu-su30.github.io/DELICIASO/"
+                rel="noreferrer"
               >
                 <img src={deliciasoImg} />
               </a>
-            </div>
-          </div>
-
-          <div className="block">
-            <div className="siteImage">
-              <a
-                target="_blank"
-                href="https://priyanshu-su30.github.io/Holi-Special/"
-              >
-                <img src={holiImg} />
-              </a>
-            </div>
-            <div className="aboutProject">
-              <h4>
-                <b>Holi-Special</b>
-              </h4>
-              <p>
-                This page is designed using bootstrap, HTML, CSS and JavaScript.
-                It is a userfriendly page with a snowfall effect and colourfull
-                background as a part of festival of colors i.e. HOLI.
-              </p>
-              <div className="techName">
-                <p>HTML</p>
-                <p>Javascript</p>
-              </div>
-              <div class="links">
-                <a
-                  href="https://github.com/Priyanshu-su30/Holi-Special"
-                  target="_blank"
-                >
-                  CODE <i className="fa fa-fw fa-github"></i>
-                </a>
-                <a
-                  target="_blank"
-                  href="https://priyanshu-su30.github.io/Holi-Special/"
-                  rel="noreferrer"
-                >
-                  Live Demo
-                  <i class="fa-solid fa-arrow-up-right-from-square link-icon"></i>
-                </a>
-              </div>
             </div>
           </div>
         </div>
