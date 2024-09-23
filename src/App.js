bdb4f83e2f5af8368ebc325639bba42ffb4dce48
Priyanshu <priyanshusingh.0601@gmai.com>
2024-09-23 12:21:39 +0530

    Remove component from app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/navbar';
-import Body from './components/body';
-import About from './components/about';
-import Project from './components/project';
-import Contact from './components/contact';
-import Footer from './components/footer';
-
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Body from "./components/body";
+import About from "./components/about";
+import Project from "./components/project";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Body/>
-      <About/>
-      <Project/>
-      <Contact/>
-      <Footer/>
+      <Navbar />
+      <Body />
+      <About />
+      <Project />
+      <Contact />
+      <Footer />
     </>
   );
 }
